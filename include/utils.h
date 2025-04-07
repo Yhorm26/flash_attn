bfe4b62559f382647cc0d4dcdda1f17f55de4b0b
Yhorm26 <136038508+Yhorm26@users.noreply.github.com>
2025-04-07 23:43:59 +0800

    dropout

--- include/utils.h
+++ include/utils.h
@@ -1,9 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 #include <cstdint>
+#include <curand_kernel.h>
 #include <cuda_fp16.h>
 
-// load data
+
 #define LDST128BITS(value) (reinterpret_cast<float4*>(&(value))[0])
 // MMA
 #define HMMA16816F32(RD0, RD1, RD2, RD3, RA0, RA1, RA2, RA3, RB0, RB1, RC0, RC1, RC2, RC3) asm volatile("mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32 {%0,  %1,  %2,  %3}, {%4, %5, %6, %7}, {%8, %9}, {%10, %11, %12, %13};\n" : "+f"(RD0), "+f"(RD1), "+f"(RD2), "+f"(RD3) : "r"(RA0), "r"(RA1), "r"(RA2), "r"(RA3), "r"(RB0), "r"(RB1), "f"(RC0), "f"(RC1), "f"(RC2), "f"(RC3))
@@ -30,6 +31,9 @@ typedef struct mykernelParamType
     int      Tr;
     int      Tc;
     float    softmax_scale;
+    float    dropout_prob;
+    unsigned long long seed;      // 随机种子
+    curandStatePhilox4_32_10_t* states; // CURAND状态指针
     int      window_size_left;
     int      window_size_right;
     float*   alibi_slopes_ptr;
