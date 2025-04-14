153f9ea8a9feebbf3751886616b8ec80268b46bd
Yhorm26 <136038508+Yhorm26@users.noreply.github.com>
2025-04-14 16:30:54 +0800

    Delete include/flash.h

--- include/flash.h
+++
@@ -1,8 +0,0 @@
-#ifndef FLASH_H
-#define FLASH_H
-#include "utils.h"
-
-template<bool Is_dropout, bool Is_causal, bool Is_local, bool Has_alibi>
-__global__ void forward_kernel(mykernelParamType param);
-
-#endif
